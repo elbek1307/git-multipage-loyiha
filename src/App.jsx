2b3b9e90018af51e2421b904a4baab144d5f6600
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-06-09 23:33:42 +0500

    endi

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import "./App.css"
 import Navbar from './Components/Navbar/Navbar'
 import { Footer } from './Components/Footer/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
    <>
-     <header>
-      <Navbar />
-     </header>
-
-     <Footer />
+    
    </>
   )
 }
--- src/Components/layout/layout.jsx
+++ src/Components/layout/layout.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import "./layout.css";
+import Navbar from '../Navbar/Navbar';
+import { Footer } from '../Footer/Footer';
 
 export const Layout = () => {
   return (
     <>
-    
+      <header>
+        <Navbar />
+      </header>
+      
+
+      <main>
+        <Outlet /> 
+      </main>
+      
+      <Footer />
     </>
   );
 }
