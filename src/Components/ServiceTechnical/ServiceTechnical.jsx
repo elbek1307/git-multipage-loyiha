21a656af53e4444e3d51b0eb6f71d62ba39eabcd
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-06-11 23:27:14 +0500

    ser tech css bitdi

--- src/Components/ServiceTechnical/ServiceTechnical.jsx
+++ src/Components/ServiceTechnical/ServiceTechnical.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./ServiceTechnical.css"
 
-import serImg from "./ser_tech_img"
+import serImg from "./ser_tech_img.png"
 
 export const ServiceTechnical = () => {
   return (
@@ -10,11 +10,11 @@ export const ServiceTechnical = () => {
     <div className="container">
         <div className="ser_technical_content">
             <span className="ser_technical_subtitle">Technical Support</span>
-            <h2 className="ser_technical_title">Best in class tech support for your <br /> company. We become your tech <br />backbone </h2>
+            <h2 className="ser_technical_title">Best in class tech support for your company. We become your tech backbone </h2>
 
-            <p className="ser_technical_text">Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every.c:\Users\abdus\Downloads\ser_tech_img.png</p>
+            <p className="ser_technical_text">Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every</p>
         </div>
-        <img src={serImg} alt="" className="ser_technical" />
+        <img src={serImg} alt="woman picture" className="ser_technical_img" />
     </div>
    </section>
    </>
--- src/Pages/ServicePages/Service.jsx
+++ src/Pages/ServicePages/Service.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import ServiceOurservices from '../../Components/ServiceOurservices/ServiceOurservices';
 import OurProcess from '../../Components/OurProcess/OurProcess';
+import { ServiceTechnical } from '../../Components/ServiceTechnical/ServiceTechnical';
 
 const Service = () => {
   return (
     <>
     <ServiceOurservices />
     <OurProcess />
+    <ServiceTechnical />
     </>
   );
 };
