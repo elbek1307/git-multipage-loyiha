7fbd48ef9dc8dfb4bbc3eeef987dc5799b123399
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-06-12 02:13:19 +0500

    xatoliklar tuzatildi

--- src/Components/CompanyTeamwork/CompanyTeamwork.jsx
+++ src/Components/CompanyTeamwork/CompanyTeamwork.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import "./CompanyTeamwork.css"
 import { FaTwitter, FaFacebookF, FaLinkedinIn } from "react-icons/fa"
 
-
 import team1 from "./team 1.png"
 import team2 from "./team 2.png"
 import team3 from "./team 3.png"
@@ -16,6 +15,7 @@ export const CompanyTeamwork = () => {
   return (
     <>
     <section className="com_team">
+
       <div className="container">
         
         <div className="com_team_content">
@@ -26,127 +26,120 @@ export const CompanyTeamwork = () => {
           </p>
         </div>
 
-      </div>
-
-     
-      <div className="com_team_grid">
-        
-        {/* 1-Xodim */}
-        <div className="com_team_member">
-          <img src={team1} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">Javena Melo</h4>
-            <span className="com_team_role">Support Assist</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+       
+        <div className="com_team_grid">
+          
+          <div className="com_team_member">
+            <img src={team1} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">Javena Melo</h4>
+              <span className="com_team_role">Support Assist</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 2-Xodim */}
-        <div className="com_team_member">
-          <img src={team2} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">Alex Smith</h4>
-            <span className="com_team_role">Developer</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+          <div className="com_team_member">
+            <img src={team2} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">Alex Smith</h4>
+              <span className="com_team_role">Developer</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 3-Xodim */}
-        <div className="com_team_member">
-          <img src={team3} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">John Doe</h4>
-            <span className="com_team_role">Designer</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+          <div className="com_team_member">
+            <img src={team3} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">John Doe</h4>
+              <span className="com_team_role">Designer</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 4-Xodim */}
-        <div className="com_team_member">
-          <img src={team4} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">Michael Kors</h4>
-            <span className="com_team_role">Manager</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+          <div className="com_team_member">
+            <img src={team4} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">Michael Kors</h4>
+              <span className="com_team_role">Manager</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 5-Xodim */}
-        <div className="com_team_member">
-          <img src={team5} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">Sarah Connor</h4>
-            <span className="com_team_role">QA Tester</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+          <div className="com_team_member">
+            <img src={team5} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">Sarah Connor</h4>
+              <span className="com_team_role">QA Tester</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 6-Xodim */}
-        <div className="com_team_member">
-          <img src={team6} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">David Miller</h4>
-            <span className="com_team_role">Frontend Dev</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+          <div className="com_team_member">
+            <img src={team6} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">David Miller</h4>
+              <span className="com_team_role">Frontend Dev</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 7-Xodim */}
-        <div className="com_team_member">
-          <img src={team7} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">Chris Evans</h4>
-            <span className="com_team_role">Backend Dev</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+          <div className="com_team_member">
+            <img src={team7} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">Chris Evans</h4>
+              <span className="com_team_role">Backend Dev</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 8-Xodim */}
-        <div className="com_team_member">
-          <img src={team8} alt="Team member" className="com_team_img" />
-          <div className="com_team_overlay">
-            <h4 className="com_team_name">Anna White</h4>
-            <span className="com_team_role">HR Manager</span>
-            <div className="com_team_socials">
-              <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
-              <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+          <div className="com_team_member">
+            <img src={team8} alt="Team member" className="com_team_img" />
+            <div className="com_team_overlay">
+              <h4 className="com_team_name">Anna White</h4>
+              <span className="com_team_role">HR Manager</span>
+              <div className="com_team_socials">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              </div>
             </div>
           </div>
+
         </div>
+    
 
       </div>
+   
       
-     
-      <div className="com_team_bottom_line"></div>
+
     </section>
     </>
   )
