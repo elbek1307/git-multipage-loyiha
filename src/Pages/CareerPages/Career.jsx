77c8fcccc5c283214df6ff1feef664f9bc682bb6
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-06-12 20:27:05 +0500

    career compnentiga ulandi jobs

--- src/Pages/CareerPages/Career.jsx
+++ src/Pages/CareerPages/Career.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Career.css"
 import CareerHero from '../../Components/CareerHero/CareerHero'
+import CareerJobs from '../../Components/CareerJobs/CareerJobs'
 
 const Career = () => {
   return (
     <>
     <CareerHero />
+    <CareerJobs />
     </>
   )
 }
